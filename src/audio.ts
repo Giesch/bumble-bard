73888e15618fce9589974f89cac455cc8da62c75
Dan Knutson <19314778+Giesch@users.noreply.github.com>
2026-07-06 14:15:41 -0400

    add audio system

---
+++ src/audio.ts
@@ -0,0 +1,32 @@
+const VOLUME = 1.0;
+
+export class AudioSystem {
+  audioCtx: AudioContext;
+  musicGain: GainNode;
+
+  constructor(audioCtx: AudioContext) {
+    this.audioCtx = audioCtx;
+    this.musicGain = this.audioCtx.createGain();
+    this.musicGain.gain.value = VOLUME;
+    this.musicGain.connect(this.audioCtx.destination);
+  }
+
+  async load(path: string): Promise<Track> {
+    const response = await fetch(path);
+    const rawBuffer = await response.arrayBuffer();
+    const buffer = await this.audioCtx.decodeAudioData(rawBuffer);
+    return { buffer, path };
+  }
+
+  play(track: Track) {
+    let source = this.audioCtx.createBufferSource();
+    source.buffer = track.buffer;
+    source.connect(this.musicGain);
+    source.start();
+  }
+}
+
+export type Track = {
+  path: string;
+  buffer: AudioBuffer;
+};
